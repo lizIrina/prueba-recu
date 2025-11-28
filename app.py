7751a455545cc6f7ed0ff54ca0ce6424ea8f5143
Nayhely Valle <ngt.valle@yavirac.edu.ec>
2025-11-28 16:28:37 -0500

    corregido

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def home():
             </style>
         </head>
         <body>
-            <h1>matth calculator</h1>
+            <h1>Irina recuperacion</h1>
             <p>hola mundo</p>
         </body>
     </html>
